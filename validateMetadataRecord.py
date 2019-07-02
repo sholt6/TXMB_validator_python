4259b9b6d480e08646e4e0c275b70e8366dc251c
sholt6 <samueljrholt@gmail.com>
2019-07-02 14:58:05 +0100

    Initial commit

---
+++ validateMetadataRecord.py
@@ -0,0 +1,329 @@
+# validateMetadataRecord.py
+
+"""
+Set of functions used to validate metadata record of taxonomic reference set
+analysis object before submission to ENA.
+"""
+
+import unittest
+import re
+
+character_regex = re.compile("^[A-Za-z0-9_]+$")
+field_length_limit = 50
+
+def validate_local_taxonomy(local_taxonomy):
+	"""Validate definition of taxonomic system
+
+	Keyword arguments:
+	local_taxonomy -- string giving name of taxonomic system
+
+	Returns:
+	taxonomy_validation_errors -- text of any errors found
+	ncbi_tax -- True or False
+	"""
+
+	field_name = 'local_taxonomy'
+	ncbi_synonyms = ('NCBI', 'ncbi')
+	ncbi_tax = False
+	taxonomy_validation_errors = []
+
+	try:
+		assert(local_taxonomy)
+	except AssertionError:
+		taxonomy_validation_errors.append(
+									get_empty_mandatory_value_error(field_name))
+		return taxonomy_validation_errors
+
+	try:
+		assert(str(local_taxonomy))
+	except AssertionError:
+		taxonomy_validation_errors.append(
+					get_data_type_error_text(field_name, local_taxonomy, 'str'))
+
+	try:
+		assert(re.match(character_regex, str(local_taxonomy)))
+	except AssertionError:
+		taxonomy_validation_errors.append(
+			get_regex_mismatch_error_text(field_name, character_regex))
+
+	try:
+		assert(len(str(local_taxonomy)) <= field_length_limit)
+	except AssertionError:
+		taxonomy_validation_errors.append(
+			get_field_length_error_text(field_name))
+
+	if (local_taxonomy in ncbi_synonyms):
+		ncbi_tax = True
+
+	return (taxonomy_validation_errors, ncbi_tax)
+
+
+def validate_local_taxonomy_version(local_taxonomy_version):
+	"""Validate version information of taxonomic system
+
+	Keyword arguments:
+	local_taxonomy_version -- user-provided version of taxonomic system
+
+	Returns:
+	local_taxonomy_version_errors -- text of any errors found
+
+	"""
+
+	field_name = "local_taxonomy_version"
+	tax_version_regex = re.compile("^[A-Za-z0-9_.]+$")
+	local_taxonomy_version_errors = []
+
+	try:
+		assert(local_taxonomy_version)
+	except AssertionError:
+		local_taxonomy_version_errors.append(
+									get_empty_mandatory_value_error(field_name))
+		return local_taxonomy_version_errors
+
+	try:
+		assert(str(local_taxonomy_version))
+	except AssertionError:
+		local_taxonomy_version_errors.append(
+					get_data_type_error_text(field_name, local_taxonomy_version,
+											 'str'))
+		return local_taxonomy_version_errors
+
+	try:
+		assert(re.match(tax_version_regex, str(local_taxonomy_version)))
+	except AssertionError:
+		local_taxonomy_version_errors.append(
+			get_regex_mismatch_error_text(field_name,
+										  tax_version_regex))
+
+	try:
+		assert(len(str(local_taxonomy_version)) <= field_length_limit)
+	except AssertionError:
+		local_taxonomy_version_errors.append(get_field_length_error_text(
+													  				field_name))
+
+	return local_taxonomy_version_errors
+
+
+def validate_dataset_name(reference_dataset_name):
+	"""Validate user-define name of analysis object
+
+	Keyword arguments:
+	reference_dataset_name -- user-defined name for this submission
+
+	Returns:
+	dataset_name_errors -- text of any errors found
+
+	"""
+
+	field_name = "reference_dataset_name"
+	reference_dataset_name_errors = []
+
+	try:
+		assert(re.match(character_regex, reference_dataset_name))
+	except AssertionError:
+		reference_dataset_name_errors.append(get_regex_mismatch_error_text(
+												field_name, character_regex))
+
+	try:
+		assert(len(reference_dataset_name) <= field_length_limit)
+	except AssertionError:
+		reference_dataset_name_errors.append(get_field_length_error_text(
+																	field_name))
+
+	return reference_dataset_name_errors
+
+
+def validate_custom_columns(input_custom_columns):
+	"""Confirms custom columns meet some minimal standards including character
+	restriction, name length, and pairing
+
+	Keyword arguments:
+	input_custom_columns -- list of user-provided column names and descriptions
+
+	Returns:
+	custom_column_name_errors -- text of any errors found
+
+	"""
+
+	custom_column_name_errors = []
+
+	try:
+		assert(len(input_custom_columns) % 2 == 0)
+	except AssertionError:
+		column_no_message = ("Number of custom columns and descriptions is not "
+		"even, indicating that some are not matched.")
+		custom_column_name_errors.append(column_no_message)
+
+	for name in input_custom_columns:
+		field_name = "custom_column " + name
+
+		try:
+			assert(re.match(character_regex, name))
+		except AssertionError:
+			custom_column_name_errors.append(get_regex_mismatch_error_text(
+												   field_name, character_regex))
+
+		try:
+			assert(len(name) <= field_length_limit)
+		except AssertionError:
+			custom_column_name_errors.append(get_field_length_error_text(
+																	field_name))
+
+	return custom_column_name_errors
+
+
+def get_regex_mismatch_error_text(field_name, source_regex):
+	"""Provides error message when entered data does not match regex defined
+	above
+
+	Keyword arguments:
+	field_name -- name of field where error was found
+
+	Returns
+	string containing error message
+	"""
+
+	return("Value entered for " + field_name +
+			" does not match regex " + source_regex.pattern)
+
+
+def get_field_length_error_text(field_name):
+	"""Provides error message when entered data is too long
+
+	Keyword arguments:
+	field_name -- name of field where error was found
+
+	Returns
+	string containing error message
+	"""
+
+	return("Value entered for " + field_name +
+			" exceeds character length limit of " + str(field_length_limit))
+
+
+def get_data_type_error_text(field_name, field_value, type_name):
+	"""Provides error message when an entered value cannot be converted to
+	required type
+
+	Keyword arguments:
+	field_name -- name of field where error was found
+	field_value -- value which was found to be in error
+	type_name -- type of value which is required
+
+	Returns
+	string containing error message
+	"""
+
+	message = ''
+
+	try:
+		message = ("Value " + str(field_value) + " entered for " + field_name +
+		   " could not be parsed as a valid " + type_name)
+	except TypeError:
+		message = "A value entered for " + field_name + " could not be read"
+
+	return message
+
+
+def get_empty_mandatory_value_error(field_name):
+	"""Provides error message for when a mandatory field is provided an empty
+	value
+
+	Keyword arguments;
+	field_name -- str name of field which was not provided
+
+	Returns
+	string containing error message
+	"""
+
+	message = "No value was given for mandatory field " + field_name
+
+	return message
+
+
+class Test(unittest.TestCase):
+	"""Test suite for module validateMetadataRecord.py"""
+	too_long_name = 'reallylongnameofatleast50charactersisthisenoughofthemyet'
+	unacceptable_characters_name = '!"£$%^&*()'
+
+	# Taxonomy name tests
+	def test_local_taxonomy_length(self):
+		local_taxonomy_case_1 = validate_local_taxonomy(self.too_long_name)
+		assert(local_taxonomy_case_1[0])
+		assert(not local_taxonomy_case_1[1])
+
+	def test_local_taxonomy_regex(self):
+		local_taxonomy_case_2 = validate_local_taxonomy(self.unacceptable_characters_name)
+		assert(local_taxonomy_case_2[0])
+		assert(not local_taxonomy_case_2[1])
+
+	def test_local_taxonomy_good_input(self):
+		local_taxonomy_case_3 = validate_local_taxonomy('A_Taxonomy_Database')
+		assert(not local_taxonomy_case_3[0])
+		assert(not local_taxonomy_case_3[1])
+
+	def test_local_taxonomy_upper_ncbi_input(self):
+		local_taxonomy_case_4 = validate_local_taxonomy('NCBI')
+		assert(not local_taxonomy_case_4[0])
+		assert(local_taxonomy_case_4[1])
+
+	def test_local_taxonomy_lower_ncbi_input(self):
+		local_taxonomy_case_5 = validate_local_taxonomy('ncbi')
+		assert(not local_taxonomy_case_5[0])
+		assert(local_taxonomy_case_5[1])
+
+	def test_local_taxonomy_integer(self):
+		local_taxonomy_case_6 = validate_local_taxonomy(6)
+		assert(not local_taxonomy_case_6[0])
+		assert(not local_taxonomy_case_6[1])
+
+	def test_local_taxonomy_empty(self):
+		local_taxonomy_case_7 = validate_local_taxonomy('')
+		assert(local_taxonomy_case_7[0])
+		assert(len(local_taxonomy_case_7) == 1)
+
+	# Taxonomy version tests
+	def test_local_taxonomy_version_length(self):
+		local_taxonomy_version_case_1 = validate_local_taxonomy_version(self.too_long_name)
+		assert(local_taxonomy_version_case_1[0])
+
+	def test_local_taxonomy_version_regex(self):
+		local_taxonomy_version_case_2 = validate_local_taxonomy_version(self.unacceptable_characters_name)
+		assert(local_taxonomy_version_case_2[0])
+
+	def test_local_taxonomy_version_integer(self):
+		local_taxonomy_version_case_3 = validate_local_taxonomy_version(3)
+		assert(not local_taxonomy_version_case_3)
+
+	def test_local_taxonomy_version_empty(self):
+		local_taxonomy_version_case_4 = validate_local_taxonomy_version('')
+		assert(local_taxonomy_version_case_4[0])
+
+	# Dataset name tests
+	def test_dataset_name_length(self):
+		dataset_name_case_1 = validate_dataset_name(self.too_long_name)
+		assert(dataset_name_case_1[0])
+
+	def test_dataset_name_regex(self):
+		dataset_name_case_2 = validate_dataset_name('$%^&*()')
+		assert(dataset_name_case_2[0])
+
+	# Custom column tests
+	long_columns = [too_long_name, too_long_name]
+	bad_name_columns = [unacceptable_characters_name, unacceptable_characters_name]
+	odd_no_of_columns = ['col1', 'desc1', 'col2']
+
+	def test_columns_long(self):
+		columns_name_case_1 = validate_custom_columns(self.long_columns)
+		assert(columns_name_case_1[0])
+
+	def test_columns_bad_name(self):
+		columns_name_case_2 = validate_custom_columns(self.bad_name_columns)
+		assert(columns_name_case_2[0])
+
+	def test_columns_odd_no(self):
+		columns_name_case_3 = validate_custom_columns(self.odd_no_of_columns)
+		assert(columns_name_case_3[0])
+
+if __name__ == '__main__':
+	unittest.main()
---
+++ validateMetadataTable.py
@@ -0,0 +1,484 @@
+# validateMetadataRecord.py
+
+"""
+Set of functions used to validate metadata table of taxonomic reference set
+analysis object before submission to ENA.
+"""
+
+import unittest
+import requests
+import re
+
+character_regex = re.compile("^[A-Za-z0-9_]+$")
+field_length_limit = 50
+
+def validate_number_of_columns(mandatory_headers, input_headers, record_custom_columns):
+	"""Checks metadata table contains correct number of columns
+
+	Keyword arguments:
+	mandatory_headers -- list of internally defined required headers
+	input_headers -- full list of headers in given table
+	record_custom_columns -- dictionary of custom column names given in metadata record
+
+	Returns:
+	column_number_errors -- list of any errors found with number of columns
+	"""
+
+	column_number_errors = []
+	message = ''
+
+	num_mandatory_headers = len(mandatory_headers)
+	num_input_headers = len(input_headers)
+	num_custom_columns = len(record_custom_columns)
+
+	num_columns_expected = num_mandatory_headers + num_custom_columns
+
+	try:
+		assert(num_columns_expected == num_input_headers)
+		message = ''
+	except AssertionError:
+		message = ("Metadata table contains {0} columns, {1} were expected. Please "
+				 "ensure all custom columns are defined in the metadata record.".format(
+				 num_input_headers, num_columns_expected))
+
+	column_number_errors.append(message)
+
+	return column_number_errors
+
+
+
+def validate_mandatory_headers(input_headers, mandatory_headers):
+	"""Checks all mandatory headers are present in the input table
+
+	Keyword arguments:
+	input_headers -- full list of headers in given table
+	mandatory_headers -- list of mandatory header names
+
+	Returns:
+	custom_headers -- list of any non-mandatory headers identified
+	mandatory_header_errors -- list of any errors found with header names
+	"""
+
+	mandatory_header_errors = []
+	custom_headers = []
+
+	for mand_index in range(0, len(mandatory_headers)):
+		try:
+			input_index = input_headers.index(mandatory_headers[mand_index])
+		except ValueError:
+			message = ("A mandatory header, '{0}', could not be found in the input"
+					   " metadata table".format(mandatory_headers[mand_index]))
+			mandatory_header_errors.append(message)
+			continue
+		mandatory_headers = mandatory_headers.pop(mand_index)
+		input_headers = input_headers.pop(input_index)
+
+	try:
+		assert(not mandatory_headers)
+	except AssertionError:
+		message = ("One or more mandatory headers were not found in the input "
+				   "table: {0}".format(mandatory_headers))
+
+	custom_headers = input_headers
+
+	return mandatory_header_errors, custom_headers
+
+
+def validate_custom_columns(table_custom_columns, record_custom_columns):
+	"""Checks custom columns from metadata record all match with custom columns
+	from metadata table
+
+	Keyword arguments:
+	table_custom_columns -- list, custom columns found in table
+	record_custom_columns -- dict, custom columns specified in metadata record
+
+	Returns:
+	custom_column_errors -- list of any errors found with custom columns
+	"""
+
+	custom_column_errors = []
+
+	record_headers = list(record_custom_columns.keys())
+
+	sorted_record_headers = record_headers.sort()
+	sorted_table_headers = record_headers.sort()
+
+	try:
+		assert(len(sorted_record_headers) == len(sorted_table_headers))
+	except AssertionError:
+		message = ("Number of custom headers is mismatched between metadata "
+				   "record and metadata table.")
+		custom_column_errors.append(message)
+
+	for record_index in range(0, len(sorted_record_headers)):
+		try:
+			input_index = sorted_table_headers.index(sorted_record_headers[record_index])
+		except ValueError:
+
+
+			##TODO - REVIEW AND FINISH THIS
+
+
+
+	return custom_column_errors
+
+
+
+
+def validate_identifier(sequence_identifier):
+	"""Checks whether an internal sequence identifier is valid. Not null.
+
+	Keyword arguments:
+	sequence_identifier -- string
+
+	Returns:
+	identifier_errors -- list of errors found with identifier
+	"""
+
+
+
+def validate_insdc_sequence_accession(insdc_sequence_accession):
+	"""Minimal checks on INSDC sequence accessions. Null input allowed.
+
+	Keyword arguments:
+	insdc_sequence_accession -- string accession
+
+	Returns:
+	accession_errors -- list of errors found with accession
+	accession_present -- bool, whether accession is present or not
+						 invalid accession still counts as accession
+	"""
+
+
+
+def validate_insdc_sequence_range(insdc_sequence_range, accession_present):
+	"""Checks that an INSDC sequence range is in appropriate format. Null input allowed.
+
+	Keyword arguments:
+	insdc_sequence_range -- string sequence range
+	accession_present -- bool, whether an accession was given
+
+	Returns:
+	range_errors -- list of errors found with range
+	"""
+
+
+
+def validate_local_organism_name(local_organism_name, ncbi_tax):
+	"""Checks that given organism name is valid. Not null.
+	Could be modified to check NCBI tax browser
+
+	Keyword arguments:
+	local_organism_name -- string, organism name
+	ncbi_tax -- bool, whether we are using NCBI tax or not
+
+	Returns:
+	organism_name_errors -- list of errors found with name
+	expected_ncbi_tax_id -- int, tax ID determined if using NCBI tax
+	"""
+
+	# can catch connection error here!
+
+def validate_local_lineage(local_lineage):
+	"""Minimal checks of organism lineage format. Null input allowed.
+	Doesn't check that lineage is appropriate to organism name.
+
+	Keyword arguments:
+	local_lineage -- string, input linage of organism
+
+	Returns:
+	local_lineage_errors -- list of errors found
+	"""
+
+
+def validate_ncbi_tax_id(input_ncbi_tax_id, expected_ncbi_tax_id, ncbi_tax):
+	"""Checks given Tax ID matches expected Tax ID, if NCBI tax in use
+
+	Keyword arguments:
+	input_ncbi_tax_id -- int, tax id from table
+	expected_ncbi_tax_id -- int, tax id from function validate_local_organism_name()
+	ncbi_tax -- bool, whether NCBI tax is being used for this submission
+
+	Returns:
+	ncbi_tax_id_errors -- list of errors found
+	"""
+
+
+class Test(unittest.TestCase):
+	"""Test suite for module validateMetadataTable.py"""
+
+	empty_list = ()
+	boolean = True
+	string = 'string'
+	integer = 6
+	too_long_name = 'reallylongnameofatleast50charactersisthisenoughofthemyet'
+	unacceptable_characters_name = '!"£$%^&*()'
+	mandatory_headers = ('Local Identifier',
+						 'INSDC Sequence Accession',
+						 'INSDC Sequence Range',
+						 'Local Organism Name',
+						 'Local Lineage',
+						 'NCBI Taxon ID')
+	correct_headers = ('Local Identifier',
+					   'INSDC Sequence Accession',
+					   'INSDC Sequence Range',
+					   'Local Organism Name',
+					   'Local Lineage',
+					   'NCBI Taxon ID')
+	missing_headers = ('Local Identifier',
+					   'INSDC Sequence Accession',
+					   'Local Lineage',
+					   'NCBI Taxon ID')
+	correct_headers_extras = ('Local Identifier',
+					 		  'INSDC Sequence Accession',
+					 		  'INSDC Sequence Range',
+					 		  'Extra Header One',
+					 	      'Local Organism Name',
+					 		  'Local Lineage',
+					 		  'Extra Header Two',
+					 		  'NCBI Taxon ID')
+	custom_columns = ('custom_column_1',
+					  'custom_column_desc_1',
+					  'custom_column_2',
+					  'custom_column_desc_2')
+
+
+	# validate_number_of_columns tests
+	def test_num_columns_correct(self):
+		num_columns_case_1 = validate_number_of_columns(self.mandatory_headers, self.correct_headers, self.custom_columns)
+		assert(not num_columns_case_1[0])
+
+	def test_num_columns_no_custom(self):
+		num_columns_case_2 = validate_number_of_columns(self.mandatory_headers, self.correct_headers, self.empty_list)
+		assert(not num_columns_case_2[0])
+
+	def test_num_columns_missing_w_custom(self):
+		num_columns_case_3 = validate_number_of_columns(self.mandatory_headers, self.missing_headers, self.custom_columns)
+		assert(num_columns_case_3[0])
+
+	def test_num_columns_missing_no_custom(self):
+		num_columns_case_4 = validate_number_of_columns(self.mandatory_headers, self.missing_headers, self.empty_list)
+		assert(num_columns_case_4[0])
+
+	def test_num_columns_wrong_kw_1(self):
+		num_columns_case_5 = validate_number_of_columns(self.mandatory_headers, self.integer, self.custom_columns)
+		assert(num_columns_case_5[0])
+
+	def test_num_columns_wrong_kw_2(self):
+		num_columns_case_6 = validate_number_of_columns(self.mandatory_headers, self.correct_headers, self.integer)
+		assert(num_columns_case_6[0])
+
+	# validate_mandatory_headers tests
+	def test_validate_mandatory_correct(self):
+		mandatory_cols_case_1 = validate_mandatory_headers(self.correct_headers, self.mandatory_headers)
+		assert(not mandatory_cols_case_1[0])
+		assert(not mandatory_cols_case_1[1])
+
+	def test_validate_mandatory_missing(self):
+		mandatory_cols_case_2 = validate_mandatory_headers(self.missing_headers, self.mandatory_headers)
+		assert(mandatory_cols_case_1[0])
+		assert(not mandatory_cols_case_2[1])
+
+	def test_validate_mandatory_reverse_mismatch(self):
+		mandatory_cols_case_3 = validate_mandatory_headers(self.mandatory_headers, self.missing_headers)
+		assert(not mandatory_cols_case_3[0])
+		assert(mandatory_cols_case_3[1])
+
+	def test_validate_mandatory_wrong_type_1(self):
+		mandatory_cols_case_4 = validate_mandatory_headers(self.integer, self.mandatory_headers)
+		assert(mandatory_cols_case_4[0])
+		assert(not mandatory_cols_case_4[1])
+
+	def test_validate_mandatory_wrong_type_2(self):
+		mandatory_cols_case_5 = validate_mandatory_headers(self.correct_headers, integer)
+		assert(mandatory_cols_case_5[0])
+		assert(not mandatory_cols_case_5[1])
+
+	# validate_identifier tests
+	def test_validate_identifier_valid(self):
+		validate_identifer_case_1 = validate_identifier(self.string)
+		assert(not validate_identifer_case_1[0])
+
+	def test_validate_identifer_empty(self):
+		validate_identifer_case_2 = validate_identifier('')
+		assert(validate_identifer_case_2[0])
+
+	def test_validate_identifier_too_long(self):
+		validate_identifer_case_3 = validate_identifier(self.too_long_name)
+		assert(validate_identifer_case_3[0])
+
+	def test_validate_identier_bad_chars(self):
+		validate_identifer_case_4 = validate_identifier(self.unacceptable_characters_name)
+		assert(validate_identifer_case_4[0])
+
+	# validate_insdc_sequence_accession tests
+	def test_validate_accession_valid(self):
+		validate_accession_case_1 = validate_identifier('LR590077')
+		assert(not validate_accession_case_1[0])
+		assert(validate_accession_case_1[1])
+
+	def test_validate_accession_invalid_string(self):
+		validate_accession_case_2 = validate_identifier(self.string)
+		assert(validate_accession_case_2[0])
+		assert(validate_accession_case_2[1])
+
+	def test_validate_accession_bad_insdc(self):
+		validate_accession_case_3 = validate_identifier('58LR0077')
+		assert(validate_accession_case_3[0])
+		assert(validate_accession_case_3[1])
+
+	def test_validate_accession_integer(self):
+		validate_accession_case_4 = validate_identifier(self.integer)
+		assert(validate_accession_case_4[0])
+		assert(validate_accession_case_4[1])
+
+	def test_validate_accession_bool(self):
+		validate_accession_case_5 = validate_identifier(self.boolean)
+		assert(validate_accession_case_5[0])
+		assert(validate_accession_case_5[1])
+
+	def test_validate_accession_null(self):
+		validate_accession_case_6 = validate_identifier('')
+		assert(not validate_accession_case_6[0])
+		assert(not validate_accession_case_6[1])
+
+	# validate_insdc_sequence_range tests
+	def test_validate_sequence_range_valid_wanted(self):
+		validate_seq_range_case_1 = validate_insdc_sequence_range('21..3523', True)
+		assert(not validate_seq_range_case_1[0])
+
+	def test_validate_sequence_range_valid_unwanted(self):
+		validate_seq_range_case_2 = validate_insdc_sequence_range('21..3523', False)
+		assert(validate_seq_range_case_2[0])
+
+	def test_validate_sequence_range_invalid_string_wanted(self):
+		validate_seq_range_case_3 = validate_insdc_sequence_range(self.string, True)
+		assert(validate_seq_range_case_3[0])
+
+	def test_validate_sequence_range_invalid_string_unwanted(self):
+		validate_seq_range_case_4 = validate_insdc_sequence_range(self.string, False)
+		assert(validate_seq_range_case_4[0])
+
+	def test_validate_sequence_range_off_format(self):
+		validate_seq_range_case_5 = validate_insdc_sequence_range('21,.3523', True)
+		assert(validate_seq_range_case_5[0])
+
+	def test_validate_sequence_range_integer(self):
+		validate_seq_range_case_6 = validate_insdc_sequence_range(self.integer, True)
+		assert(validate_seq_range_case_6[0])
+
+	def test_validate_sequence_range_bool(self):
+		validate_seq_range_case_7 = validate_insdc_sequence_range(self.boolean, True)
+		assert(validate_seq_range_case_7[0])
+
+	def test_validate_sequence_range_null_wanted(self):
+		validate_seq_range_case_8 = validate_insdc_sequence_range('', True)
+		assert(validate_seq_range_case_8[0])
+
+	def test_validate_sequence_range_null_unwanted(self):
+		validate_seq_range_case_9 = validate_insdc_sequence_range('', False)
+		assert(not validate_seq_range_case_9[0])
+
+	# validate_local_organism_name tests
+	def test_validate_organism_name_human_ncbi(self):
+		validate_organism_name_case_1 = validate_local_organism_name('Homo sapiens', True)
+		assert(not validate_organism_name_case_1[0])
+		assert(validate_organism_name_case_1[1] == '9606')
+
+	def test_validate_organism_name_human_not_ncbi(self):
+		validate_organism_name_case_2 = validate_local_organism_name('Homo sapiens', False)
+		assert(not validate_organism_name_case_2[0])
+		assert(not validate_organism_name_case_2[1])
+
+	def test_validate_organism_name_stram_ncbi(self):
+		validate_organism_name_case_3 = validate_local_organism_name('Stramenopiles sp. MAST-7 TOSAG23-7', True)
+		assert(not validate_organism_name_case_3[0])
+		assert(validate_organism_name_case_3[1] == '2590674')
+
+	def test_validate_organism_name_stram_not_ncbi(self):
+		validate_organism_name_case_4 = validate_local_organism_name('Stramenopiles sp. MAST-7 TOSAG23-7', False)
+		assert(not validate_organism_name_case_4[0])
+		assert(not validate_organism_name_case_4[1])
+
+	def test_validate_organism_name_fake_ncbi(self): # fake name with ncbi validation
+		validate_organism_name_case_5 = validate_local_organism_name('Schmlub gablub', True)
+		assert(validate_organism_name_case_5[0])
+		assert(not validate_organism_name_case_5[1])
+
+	def test_validate_organism_name_fake_non_ncbi(self): # fake name without ncbi validation
+		validate_organism_name_case_6 = validate_local_organism_name('Schmlub gablub', False)
+		assert(not validate_organism_name_case_6[0])
+		assert(not validate_organism_name_case_6[1])
+
+	def test_validate_organism_name_integer(self):
+		validate_organism_name_case_7 = validate_local_organism_name(self.integer, True)
+		assert(validate_organism_name_case_7[0])
+		assert(not validate_organism_name_case_7[1])
+
+	def test_validate_organism_name_bool(self):
+		validate_organism_name_case_8 = validate_local_organism_name(self.boolean, True)
+		assert(validate_organism_name_case_8[0])
+		assert(not validate_organism_name_case_8[1])
+
+	def test_validate_organism_name_null(self):
+		validate_organism_name_case_9 = validate_local_organism_name('', True)
+		assert(validate_organism_name_case_9[0])
+		assert(not validate_organism_name_case_9[1])
+
+	# validate_local_lineage tests
+	def test_validate_lineage_human(self):
+		validate_lineage_case_1 = validate_local_lineage('cellular organisms; Eukaryota; Opisthokonta; Metazoa; Eumetazoa; Bilateria; Deuterostomia; Chordata; Craniata; Vertebrata; Gnathostomata; Teleostomi; Euteleostomi; Sarcopterygii; Dipnotetrapodomorpha; Tetrapoda; Amniota; Mammalia; Theria; Eutheria; Boreoeutheria; Euarchontoglires; Primates; Haplorrhini; Simiiformes; Catarrhini; Hominoidea; Hominidae; Homininae; Homo')
+		assert(not validate_lineage_case_1[0])
+
+	def test_validate_lineage_ecoli(self):
+		validate_lineage_case_2 = validate_local_lineage('cellular organisms; Bacteria; Proteobacteria; Gammaproteobacteria; Enterobacterales; Enterobacteriaceae; Escherichia')
+		assert(not validate_lineage_case_2[0])
+
+	def test_validate_lineage_string(self):
+		validate_lineage_case_3 = validate_local_lineage(self.string)
+		assert(not validate_lineage_case_3[0])
+
+	def test_validate_lineage_integer(self):
+		validate_lineage_case_4 = validate_local_lineage(self.integer)
+		assert(validate_lineage_case_4[0])
+
+	def test_validate_lineage_bool(self):
+		validate_lineage_case_5 = validate_local_lineage(self.boolean)
+		assert(not validate_lineage_case_5[0])
+
+	def test_validate_lineage_null(self):
+		validate_lineage_case_6 = validate_local_lineage('')
+		assert(not validate_lineage_case_6[0])
+
+	# validate_ncbi_tax_id tests
+	def test_validate_tax_id_valid_w_ncbi(self):
+		validate_tax_id_case_1 = validate_ncbi_tax_id(self.integer, self.integer, True)
+		assert(validate_tax_id_case_1[0])
+
+	def test_validate_tax_id_valid_wo_ncbi(self):
+		validate_tax_id_case_2 = validate_ncbi_tax_id(self.integer, self.integer, False)
+		assert(not validate_tax_id_case_2[0])
+
+	def test_validate_tax_id_invalid_w_ncbi(self):
+		validate_tax_id_case_3 = validate_ncbi_tax_id(self.integer, (self.integer + 1, True))
+		assert(validate_tax_id_case_3[0])
+
+	def test_validate_tax_id_invalid_wo_ncbi(self):
+		validate_tax_id_case_4 = validate_ncbi_tax_id(self.integer, (self.integer + 1, False))
+		assert(validate_tax_id_case_4[0])
+
+	def test_validate_tax_id_null_1(self):
+		validate_tax_id_case_5 = validate_ncbi_tax_id(0, self.integer, True)
+		assert(validate_tax_id_case_5[0])
+
+	def test_validate_tax_id_null_2(self):
+		validate_tax_id_case_ = validate_ncbi_tax_id(self.integer, 0, True)
+		assert(validate_tax_id_case_[0])
+
+	def test_validate_tax_id_null_3(self):
+		validate_tax_id_case_ = validate_ncbi_tax_id(self.integer, self.integer, 0)
+		assert(validate_tax_id_case_[0])
+
+	def test_validate_tax_id_null_all(self):
+		validate_tax_id_case_ = validate_ncbi_tax_id(0, 0, 0)
+		assert(not validate_tax_id_case_[0])
