10cbf56d20983c28cc2d25c02933649cf61df0c7
sholt6 <samueljrholt@gmail.com>
2019-07-12 16:42:00 +0100

    Fixes indicated by test suite

--- validateFasta.py
+++ validateFasta.py
@@ -5,18 +5,18 @@ Set of functions used to validate FASTA file of taxonomic reference set
 analysis object before submission to ENA.
 """
 
-import unittests
+import unittest
 import re
 import gzip
 
 
-def validate_txmb_fasta(file_name, local_identifiers):
+def validate_txmb_fasta(file_name, table_identifiers):
 	"""Main function for validating the FASTA component of a taxonomic
 	reference set
 
 	Keyword arguments:
 	file_name -- str, name of file to be checked
-	local_identifiers -- list of identifiers taken from TSV
+	table_identifiers -- list of identifiers taken from TSV
 
 	Returns:
 	fasta_errors -- list of errors found with file
@@ -25,7 +25,7 @@ def validate_txmb_fasta(file_name, local_identifiers):
 	fasta_errors = []
 	linecount = 0
 	line_flag = ''
-	remaining_ids = input_ids.copy()
+	remaining_ids = table_identifiers.copy()
 
 	try:
 		fasta_file = gzip.open(file_name, 'rt')
@@ -42,7 +42,7 @@ def validate_txmb_fasta(file_name, local_identifiers):
 		if line[0] == '>':
 			if line_flag == 'se' or line_flag =='':
 				line_flag = 'id'
-				id_line_errors, id_index = check_identifier(line, remaining_identifiers, linecount)
+				id_line_errors, id_index = check_identifier(line, remaining_ids, linecount)
 				remaining_ids.pop(id_index)
 				fasta_errors.extend(id_line_errors)
 			elif line_flag == 'id':
@@ -54,30 +54,34 @@ def validate_txmb_fasta(file_name, local_identifiers):
 				line_flag = 'se'
 				seq_line_errors = check_sequence(line, linecount)
 				fasta_errors.extend(seq_line_errors)
-			if line_flag == 'se':
+			elif line_flag == 'se':
 				message = ("Two consecutive sequence lines in FASTA at line {0}"
 						   .format(linecount))
 				fasta_errors.append(message)
 
-	if remaining_identifiers:
+	if remaining_ids:
 		message = ("The following identifiers found in the metadata table "
 				   "were not found in the FASTA file:\n")
-		for identifier in remaining_identifiers:
+		for identifier in remaining_ids:
 			message = message + identifier + "\n"
 			fasta_errors.append(message)
+
+	if (linecount % 2 != 0):
+		message = ("Input FASTA does not contain an even number of lines")
+		fasta_errors.append(message)
 
 	fasta_file.close()
 
 	return fasta_errors
 
 
-def check_identifier(id_line, remaining_identifiers, linecount):
+def check_identifier(id_line, remaining_ids, linecount):
 	"""Takes a line recognised as an ID line and checks for an identifier
 	which matches one found in the metadata TSV
 
 	Keyword arguments:
 	id_line -- str, full ID line found in FASTA file
-	remaining_identifiers -- list, identifiers not yet found in file
+	remaining_ids -- list, identifiers not yet found in file
 	linecount -- current line in file
 
 	Returns:
@@ -93,7 +97,7 @@ def check_identifier(id_line, remaining_identifiers, linecount):
 	id_index = 0
 
 	try:
-		id_index = remaining_identifiers.index(identifier)
+		id_index = remaining_ids.index(identifier)
 	except ValueError:
 		message = ("FASTA identifier '{0}' at line {1} does not match anything"
 				   " in metadata table".format(identifier, linecount))
@@ -121,7 +125,7 @@ def check_sequence(sequence_line, linecount):
 	if re.match(nucleotide_regex, sequence_line):
 		return sequence_errors
 
-	if re.match(r"\s"):
+	if re.match(r"\s", sequence_line):
 		message = ("Sequence at line {0} contains whitespace".format(linecount))
 		sequence_errors.append(message)
 
@@ -135,13 +139,18 @@ def check_sequence(sequence_line, linecount):
 			       .format(linecount))
 		sequence_errors.append(message)
 
+	if not sequence_errors:
+		message = ("Sequence errors at line {0}, could not be diagnosed"
+				   .format(linecount))
+		sequence_errors.append(message)
+
 	return sequence_errors
 
 
 class Test(unittest.TestCase):
 	"""Test suite for module validateFasta.py"""
 
-	local_identifiers = ('ITS1DB00887249',
+	local_identifiers = ['ITS1DB00887249',
 						 'ITS1DB00944432',
 						 'ITS1DB01095025',
 						 'ITS1DB01019240',
@@ -150,9 +159,9 @@ class Test(unittest.TestCase):
 						 'ITS1DB00588024',
 						 'ITS1DB00588025',
 						 'ITS1DB00588022',
-						 'ITS1DB00588023')
+						 'ITS1DB00588023']
 
-	local_identifiers_missing = ('ITS1DB00887249',
+	local_identifiers_missing = ['ITS1DB00887249',
 								 'ITS1DB00944432',
 								 'ITS1DB01095025',
 								 'ITS1DB01019240',
@@ -160,7 +169,7 @@ class Test(unittest.TestCase):
 								 'ITS1DB00588027',
 								 'ITS1DB00588024',
 								 'ITS1DB00588025',
-								 'ITS1DB00588022')
+								 'ITS1DB00588022']
 
 	# validate_txmb_fasta tests
 	def test_fasta_validation_valid(self):
@@ -205,43 +214,43 @@ class Test(unittest.TestCase):
 	def test_missing_pipe_full_line(self):
 		test_id_line_case_2 = check_identifier('>ITS1DB00588023175245uncultured fungusITS1 located by HMM annotation, 142bp', self.local_identifiers, 1)
 		assert(test_id_line_case_2[0])
-		assert(not test_id_line_case_1[1])
+		assert(not test_id_line_case_2[1])
 
 	def test_missing_pipe_only_id(self):
 		test_id_line_case_3 = check_identifier('>ITS1DB00588023', self.local_identifiers, 1)
 		assert(not test_id_line_case_3[0])
-		assert(test_id_line_case_1[1])
+		assert(test_id_line_case_3[1])
 
 	def test_only_id(self):
 		test_id_line_case_4 = check_identifier('>ITS1DB00588023|', self.local_identifiers, 1)
 		assert(not test_id_line_case_4[0])
-		assert(test_id_line_case_1[1])
+		assert(test_id_line_case_4[1])
 
 	def test_id_not_in_list(self):
 		test_id_line_case_5 = check_identifier('>ITS1DB99999999|175245|uncultured fungus|ITS1 located by HMM annotation, 142bp', self.local_identifiers, 1)
 		assert(test_id_line_case_5[0])
-		assert(not test_id_line_case_1[1])
+		assert(not test_id_line_case_5[1])
 
 
 	# check_sequence tests
 	def test_seq_check_valid(self):
-		test_seq_line_case_1 = check_sequence('gtcgaaccctgcgatagcagacgacccggtaacatgtaaacacatcgggagagcatcaaggggcatcgatcccttgaccgcgaaccctaggttggtgtccgcttagattctaaggggacgccgattgacataaccatccgggcgcggcatgcgccaaggaacttaaaatcgaattgtatgttcgcttcccgttatcgggcagcagcgtcattccaaaaaacaacg')
+		test_seq_line_case_1 = check_sequence('gtcgaaccctgcgatagcagacgacccggtaacatgtaaacacatcgggagagcatcaaggggcatcgatcccttgaccgcgaaccctaggttggtgtccgcttagattctaaggggacgccgattgacataaccatccgggcgcggcatgcgccaaggaacttaaaatcgaattgtatgttcgcttcccgttatcgggcagcagcgtcattccaaaaaacaacg', 1)
 		assert(not test_seq_line_case_1)
 
 	def test_seq_check_bad_chars(self):
-		test_seq_line_case_2 = check_sequence('abcdefghijklmnopqrstuvwxyz!£$%^&*()')
+		test_seq_line_case_2 = check_sequence('abcdefghijklmnopqrstuvwxyz!£$%^&*()', 1)
 		assert(test_seq_line_case_2[0])
 
 	def test_seq_check_newline(self):
-		test_seq_line_case_3 = check_sequence('actg\nactg')
+		test_seq_line_case_3 = check_sequence('actg\nactg', 1)
 		assert(test_seq_line_case_3[0])
 
 	def test_seq_check_spaces(self):
-		test_seq_line_case_4 = check_sequence('actg actg')
+		test_seq_line_case_4 = check_sequence('actg actg', 1)
 		assert(test_seq_line_case_4[0])
 
 	def test_seq_check_null(self):
-		test_seq_line_case_5 = check_sequence('')
+		test_seq_line_case_5 = check_sequence('', 1)
 		assert(test_seq_line_case_5[0])
 
 
